872c9841b76324b612d92096b03c169fede57b1e
albieduffy <albie.duffy@gmail.com>
2020-11-25 13:49:13 +0000

    formatting

--- lib/app.py
+++ lib/app.py
@@ -7,7 +7,7 @@ class CLI:
     def start(self):
         priority_queue = MinHeap()
         while priority_queue.get_count() == 0:
-            print('The queue is currently empty.')
+            print('The queue is currently empty.\n')
             self.add_new_task(priority_queue)
         while priority_queue.get_count() > 0:
             print("""
@@ -18,9 +18,9 @@ class CLI:
 
             """.format(priority_queue.get_current_task()))
             options = ['add', 'done']
-            user_choice = input('Select an option ("add" to add new task, "done" to complete current task): ')
+            user_choice = input('Select an option ("add" to add new task, "done" to complete current task): \n')
             while user_choice not in options:
-                user_choice = input('Please select a valid option (add or done): ')
+                user_choice = input('Please select a valid option (add or done): \n')
             if user_choice == 'add':
                 self.add_new_task(priority_queue)
             else:
@@ -28,7 +28,7 @@ class CLI:
 
     def add_new_task(self, priority_queue):
         new_task = input('Add a task: ')
-        task_priority = input('Set a priority for this task (1-5): ')
+        task_priority = input('Set a priority for this task (1-5): \n')
         priority_queue.add(Node(task_priority, new_task))
 
     def complete_task(self, priority_queue):
