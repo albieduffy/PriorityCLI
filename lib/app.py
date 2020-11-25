23c2122c483a514bea1759e68075af85ffce2c39
albieduffy <albie.duffy@gmail.com>
2020-11-25 13:51:49 +0000

    formatting

--- lib/app.py
+++ lib/app.py
@@ -12,25 +12,25 @@ class CLI:
         while priority_queue.get_count() > 0:
             print("""
 
-            Your current task is:
+            CURRENT TASK:
 
             {}
 
             """.format(priority_queue.get_current_task()))
             options = ['add', 'done']
-            user_choice = input('Select an option ("add" to add new task, "done" to complete current task): \n')
+            user_choice = input('\nSelect an option ("add" to add new task, "done" to complete current task): ')
             while user_choice not in options:
-                user_choice = input('Please select a valid option (add or done): \n')
+                user_choice = input('\nPlease select a valid option (add or done): ')
             if user_choice == 'add':
                 self.add_new_task(priority_queue)
             else:
                 self.complete_task(priority_queue)
 
     def add_new_task(self, priority_queue):
-        new_task = input('Add a task: ')
-        task_priority = input('Set a priority for this task (1-5): \n')
+        new_task = input('\nAdd a task: ')
+        task_priority = input('Set a priority for this task (1-5): ')
         priority_queue.add(Node(task_priority, new_task))
 
     def complete_task(self, priority_queue):
         completed_task = priority_queue.retrieve_min().get_task()
-        print('{} completed.'.format(completed_task))
+        print('\n{} completed.'.format(completed_task))
